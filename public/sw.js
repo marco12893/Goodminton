39df786360f28bca665485e79e7205224743a143
Marco Alexander Tejo <marcoalexandertejo@gmail.com>
2026-03-05 14:22:49 +0700

    Initial commit: Next.js project with Goodminton application

---
+++ public/sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = "goodminton-v1";
+const URLS_TO_CACHE = ["/", "/manifest.webmanifest", "/favicon.ico"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+          return undefined;
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) return cachedResponse;
+
+      return fetch(event.request)
+        .then((response) => {
+          const responseToCache = response.clone();
+          caches
+            .open(CACHE_NAME)
+            .then((cache) => cache.put(event.request, responseToCache));
+          return response;
+        })
+        .catch(() => caches.match("/"));
+    })
+  );
+});
